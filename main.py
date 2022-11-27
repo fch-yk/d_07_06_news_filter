7f4e7ccaf5f7f6cc7ff2006a61ec920dbcf794a6
yk <yefim_korshever@mail.ru>
2022-11-27 14:59:32 +0300

    Process aiohttp.ClientError

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from enum import Enum
 
 import aiofiles
 import aiohttp
@@ -10,6 +11,11 @@ import text_tools
 from adapters import SANITIZERS
 
 
+class ProcessingStatus(Enum):
+    OK = 'OK'
+    FETCH_ERROR = 'FETCH_ERROR'
+
+
 async def fetch(session, url):
     async with session.get(url) as response:
         response.raise_for_status()
@@ -17,18 +23,30 @@ async def fetch(session, url):
 
 
 async def process_article(url, charged_words, articles_cards):
-    async with aiohttp.ClientSession() as session:
-        article_text = await fetch(session, url)
-        article_text = SANITIZERS['inosmi_ru'](article_text, True)
-        morph = pymorphy2.MorphAnalyzer()
-        words = text_tools.split_by_words(morph, article_text)
-        jaundice_rate = text_tools.calculate_jaundice_rate(
-            words,
-            charged_words
-        )
-        articles_cards.append(
-            {'url': url, 'rating': jaundice_rate, 'words_number': len(words)}
-        )
+    status = ProcessingStatus.OK
+    rating = words_number = None
+    try:
+        async with aiohttp.ClientSession() as session:
+            article_text = await fetch(session, url)
+            article_text = SANITIZERS['inosmi_ru'](article_text, True)
+            morph = pymorphy2.MorphAnalyzer()
+            words = text_tools.split_by_words(morph, article_text)
+            rating = text_tools.calculate_jaundice_rate(
+                words,
+                charged_words
+            )
+            status = ProcessingStatus.OK
+            words_number = len(words)
+    except (aiohttp.ClientError,):
+        status = ProcessingStatus.FETCH_ERROR
+
+    articles_cards.append(
+        {
+            'url': url,
+            'status': status,
+            'rating': rating,
+            'words_number': words_number}
+    )
 
 
 async def main():
@@ -41,8 +59,8 @@ async def main():
                 charged_words.append(word.strip())
 
     test_articles = [
-        'https://inosmi.ru/20221106/kosmos-257523048.html',
-        'https://inosmi.ru/20221106/virusy-257514193.html',
+        'https://inosmi.ru/not/exist.html',
+        'https://inosmiy.ru/20221106/virusy-257514193.html',
         'https://inosmi.ru/20221106/videoigry-257474918.html',
         'https://inosmi.ru/20221106/kosmos-257489166.html',
         'https://inosmi.ru/20221104/mars-257472040.html'
@@ -61,6 +79,7 @@ async def main():
     for article_card in articles_cards:
         print(
             f'URL: {article_card["url"]}',
+            f'Status: {article_card["status"]}',
             f'Rating: {article_card["rating"]}',
             f'Words in the article: {article_card["words_number"]}',
             sep='\n',
